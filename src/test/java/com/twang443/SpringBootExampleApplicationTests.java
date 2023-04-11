df672c3b04b8d9b4e117c5ddb85e23c6ac043d9d
TingSyuanWang <vincent@fishboneapps.com>
2023-04-11 16:13:21 -0400

    first commit

---
+++ src/test/java/com/twang443/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twang443;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
